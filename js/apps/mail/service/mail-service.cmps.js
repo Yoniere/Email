13cc48b720bf4566c2134cdc5632db799c181d46
Yonatan Erez <jontaner12@gmail.com>
2022-03-02 15:30:56 +0200

    initials render emails 2

---
+++ js/apps/mail/service/mail-service.cmps.js
@@ -0,0 +1,47 @@
+import { storageService } from '../../../main-services/async-storage.services.js'
+import { utilService } from '../../../main-services/util.services.js';
+
+const EMAILS_KEY = 'emailsDB'
+_createEmails()
+
+export const mailService = {
+    query,
+
+}
+
+function query() {
+    return storageService.query(EMAILS_KEY)
+}
+
+function _createEmails() {
+    let emails = utilService.loadFromStorage(EMAILS_KEY);
+    if (!emails || !emails.length) {
+        emails = [{
+                id: storageService.makeId(),
+                subject: 'miss you',
+                body: 'Would love to catch up sometimes',
+                isRead: false,
+                sentAt: Date.now(),
+                to: 'momo@momo.com',
+            },
+            {
+                id: storageService.makeId(),
+                subject: 'job interview',
+                body: 'dear sir, we would like to have a short phone call regard your job application',
+                isRead: false,
+                sentAt: Date.now() + 20000,
+                to: 'momo@momo.com',
+            },
+            {
+                id: storageService.makeId(),
+                subject: 'gym contract',
+                body: 'Dear Appsus, please find attached signed gym contract. hope you see you soon',
+                isRead: false,
+                sentAt: Date.now() + 40000,
+                to: 'momo@momo.com',
+            }
+        ]
+        utilService.saveToStorage(EMAILS_KEY, emails)
+    }
+    return emails;
+}
