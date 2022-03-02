684c0314c9bc5e46b0a1650298258447105a2c48
Yonatan Erez <jontaner12@gmail.com>
2022-03-02 18:56:21 +0200

    render + delete added

--- js/router.js
+++ js/router.js
@@ -1,6 +1,7 @@
 import homePage from './views/home-page.cmp.js';
 import noteInd from './apps/keep/pages/note-index.cmp.js';
 import mailApp from './apps/mail/mail-app.cmp.js';
+import mailDetails from './apps/mail/pages/mail-details.cmp.js';
 
 
 const routes = [{
@@ -17,10 +18,10 @@ const routes = [{
         path: '/mail',
         component: mailApp
     },
-    // {
-    //     path: '/book/:bookId',
-    //     component: bookDetails
-    // },
+    {
+        path: '/mail/:mailId',
+        component: mailDetails
+    },
     // {
     //     path: '/book-add',
     //     component: addBook
